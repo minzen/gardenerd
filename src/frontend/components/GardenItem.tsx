de03f8eeff7c130c3cd6780a9f9a10b81f42bdf0
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2020-03-28 23:31:10 +0100

    Show the garden item edit form only on a button click, and hide it again upon a second click

--- src/frontend/components/GardenItem.tsx
+++ src/frontend/components/GardenItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import {
   Card,
   CardContent,
@@ -36,34 +36,55 @@ export const GardenItem = (props: GardenItemProps) => {
   const classes = useStyles()
   const [title, setTitle] = useState(props.name)
   const [description, setDescription] = useState(props.description)
+  const [editFormVisible, setEditFormVisible] = useState(false)
 
   const handleEditItem = () => {
     console.log('Edit clicked on item', props.name)
+    setEditFormVisible(!editFormVisible)
+  }
+
+  const editForm = () => {
+    if (editFormVisible) {
+      return (
+        <GardenItemForm
+          name={title}
+          description={description}
+          plantationDate={'2020-03-28'}
+          setName={setTitle}
+          setDescription={setDescription}
+        />
+      )
+    }
   }
 
   return (
     <>
-    <Card className={classes.root} variant='outlined'>
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color='textSecondary'
-          gutterBottom
-        >
-          {title}
-        </Typography>
-        <Typography variant='h5' component='h2'>
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size='small' variant='contained' color='primary' onClick={handleEditItem}>
-          Edit plant information
-        </Button>
-      </CardActions>
-    </Card>
+      <Card className={classes.root} variant='outlined'>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color='textSecondary'
+            gutterBottom
+          >
+            {title}
+          </Typography>
+          <Typography variant='h5' component='h2'>
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size='small'
+            variant='contained'
+            color='primary'
+            onClick={handleEditItem}
+          >
+            Edit plant information
+          </Button>
+        </CardActions>
+      </Card>
 
-    <GardenItemForm name={title} description={description} plantationDate={'2020-03-28'} setName={setTitle} setDescription={setDescription} />
+      {editForm()}
     </>
   )
 }
