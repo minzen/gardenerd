367ab32a91dde9cfcfa382fbca8bac42c2018a90
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2020-04-03 23:31:33 +0200

    Adds firebase authentication API items

--- src/frontend/components/Firebase/firebase.ts
+++ src/frontend/components/Firebase/firebase.ts
@@ -1,4 +1,5 @@
 import app from 'firebase/app'
+import 'firebase/auth'
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -6,12 +7,31 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 }
 
 class Firebase {
-    constructor() {
-        app.initializeApp(config)
-    }
+  auth: app.auth.Auth
+  constructor() {
+    app.initializeApp(config)
+    this.auth = app.auth()
+  }
+
+  doCreateUserWithEmailAndPassword = (email: string, password: string) => {
+    this.auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  doSignInWithEmailAndPassword = (email: string, password: string) => {
+    this.auth.signInWithEmailAndPassword(email, password)
+  }
+
+  doSignOut = () => {
+    this.auth.signOut()
+  }
+  doPasswordReset = (email: string) => this.auth.sendPasswordResetEmail(email)
+
+  doPasswordUpdate = (password: string) => {
+    this.auth.currentUser ? this.auth.currentUser.updatePassword(password) : Promise.resolve()
+  }
 }
 export default Firebase
